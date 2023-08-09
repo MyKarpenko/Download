0830f302bbabbeb76f68d3efe02b3a89a7f27850
Michael Karpenko <43587427+MyKarpenko@users.noreply.github.com>
2023-08-09 19:25:34 -0400

    Create brent_job.sql

---
+++ brent_job.sql
@@ -0,0 +1,78 @@
+USE [msdb]
+GO
+
+/****** Object:  Job [SQL_Maint: Log sp_blitz_first]    Script Date: 8/9/2023 7:24:50 PM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 8/9/2023 7:24:50 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'SQL_Maint: Log sp_blitz_first', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'https://www.brentozar.com/archive/2019/02/video-how-to-capture-baselines-with-sp_blitzfirst/    https://www.brentozar.com/askbrent/', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [run]    Script Date: 8/9/2023 7:24:50 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'run', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXEC dbadmin..sp_BlitzFirst 
+  @ExpertMode = 1,
+  @OutputDatabaseName = ''dbadmin'', 
+  @OutputSchemaName = ''dbo'', 
+  @OutputTableName = ''BlitzFirst'',
+  @OutputTableNameFileStats = ''BlitzFirst_FileStats'',
+  @OutputTableNamePerfmonStats = ''BlitzFirst_PerfmonStats'',
+  @OutputTableNameWaitStats = ''BlitzFirst_WaitStats'',
+  @OutputTableNameBlitzCache = ''BlitzCache'',
+  @OutputTableRetentionDays = 1,
+  @OutputType = ''none''', 
+		@database_name=N'dbadmin', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'15 min', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=4, 
+		@freq_subday_interval=15, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20230301, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959, 
+		@schedule_uid=N'c1c2cf7e-042a-4eb4-bb62-2d853ff80b2e'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
+
