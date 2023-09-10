ca69741e3db8becd97d223dd257eea5697f34e96
Michael Karpenko <43587427+MyKarpenko@users.noreply.github.com>
2023-09-10 18:17:08 -0400

    Update monitoring_scripts.sql

--- monitoring_scripts.sql
+++ monitoring_scripts.sql
@@ -17,11 +17,8 @@ EXEC dbadmin..sp_blitzfirst @outputtype = 'Top10', @SinceStartup = 1
 EXEC dbadmin..sp_BlitzFirst @ExpertMode=1, @Seconds=60;
 GO
 
-
-
--- SOS_SCHEDULER_YIELD
-exec dbadmin..sp_BlitzCache  @ExpertMode = 1, @HideSummary=1, @sortOrder='cpu'
-, @SkipAnalysis = 1sp_BlitzWho @ExpertMode=1, @ShowActualParameters = 1
+  
+sp_BlitzWho @ExpertMode=1, @ShowActualParameters = 1
 
 
 
@@ -39,4 +36,32 @@ EXEC dbadmin..sp_BlitzFirst
 
 
 exec dbadmin..sp_Blitz @OutputType = 'MARKDOWN', @CheckServerInfo = 1;
+
+-- https://www.joshthecoder.com/2020/08/27/unusual-threadpool-waits.html
+
+EXEC dbo.sp_PressureDetector @what_to_check = N'cpu';
+
+---------------
+
+--LCK% long running + low cpu = locking   Long Running With Low CPU
+EXEC dbadmin..sp_BlitzCache  @ExpertMode = 1, @HideSummary=1, @sortOrder='duration'
+-- resource semaphore
+exec dbadmin..sp_BlitzCache  @ExpertMode = 1, @HideSummary=1, @sortOrder='memory grant'
+-- SOS_SCHEDULER_YIELD
+EXEC dbadmin..sp_BlitzCache  @ExpertMode = 1, @HideSummary=1, @sortOrder='cpu'
+--, @SkipAnalysis = 1
+
+-- PAGEIOLATCH  THREADPOOL (queries which do scans)
+exec dbadmin..sp_BlitzCache  @ExpertMode = 1, @HideSummary=1, @sortOrder='reads'
+--, @SkipAnalysis = 1
+
+-- WRITELOG
+exec dbadmin..sp_BlitzCache  @ExpertMode = 1, @HideSummary=1, @sortOrder='writes'
+--most intencive / heavy
+exec dbadmin..sp_BlitzCache  @ExpertMode = 1, @HideSummary=1, @sortOrder='executions'
+--recent compilation
+exec dbadmin..sp_BlitzCache @SortOrder = 'recent compilations'
+
+--spills
+exec dbadmin..sp_BlitzCache  @ExpertMode = 1, @HideSummary=1, @sortOrder='spills'
 
